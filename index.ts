c78adfde132eec75d93334d13cf26935bc96e921
hhenriques1999 <hhenriques1999@gmail.com>
2023-07-18 22:48:04 -0300

    Criar getter e setter

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 class Car {
-    id: number;
+    #id: number;
     color: string;
     isElectric: boolean;
 
     constructor(id: number, color: string, isElectric: boolean) {
-        this.id = id;
+        this.#id = id;
         this.color = color;
         this.isElectric = isElectric;
+    }
+
+    getId() : number {
+        return this.#id;
+    }
+
+    setId(id:number) : void {
+        this.#id = id;
     }
 }
 
 
 const d : Car = new Car(5, "Green", false);
 
-console.log(d);
+console.log(d.getId());
+d.setId(3);
+console.log(d.getId());
